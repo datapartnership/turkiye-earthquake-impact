e63889d6b495d3ecad2b4528166a791f3202a2a5
Rob Marty <ramarty@email.wm.edu>
2023-03-27 09:54:30 -0400

    Ntl (#12)
    * ntl
    
    * update links
    
    * Add Nighttime Lights notebook to table of contents
    
    * add figures
    
    * Fix figure scale
    
    * update readme
    
    * ntl readme
    
    * readme
    
    * agg
    
    * readme
    
    * typos
    
    ---------
    
    Co-authored-by: Gabriel Stefanini Vicente <gvicente@worldbank.org>

--- notebooks/nighttime-lights/02_download_black_marble.R
+++ notebooks/nighttime-lights/02_download_black_marble.R
@@ -10,8 +10,8 @@ BEARER <- read_csv("~/Desktop/bearer_bm.csv") %>% pull(token)
 # there.
 
 ## Grab polygons
-syr_sp <- gadm(country = "SYR", level=0, path = tempdir()) 
-tur_sp <- gadm(country = "TUR", level=0, path = tempdir()) 
+syr_sp <- gadm(country = "SYR", level=0, path = tempdir())
+tur_sp <- gadm(country = "TUR", level=0, path = tempdir())
 
 ## Combine Syria and Turkey into one polygon
 roi_sp <- rbind(syr_sp, tur_sp)
@@ -19,103 +19,27 @@ roi_sp$id <- 1
 roi_sp <- raster::aggregate(roi_sp, by = "id")
 roi_sf <- roi_sp %>% st_as_sf()
 
-# Download annual data ---------------------------------------------------------
-# Downloads a raster for each month. If raster already created, skips calling 
-# function.
+# Download data ----------------------------------------------------------------
+## Annual Data
+bm_raster(roi_sf = roi_sf,
+          product_id = "VNP46A4",
+          date = 2012:2021,
+          bearer = BEARER,
+          output_location_type = "file",
+          file_dir = file.path(ntl_bm_dir, "FinalData", "VNP46A4_rasters"))
 
-years <- 2012:2021
+## Monthly Data
+bm_raster(roi_sf = roi_sf,
+          product_id = "VNP46A3",
+          date = seq.Date(from = ymd("2022-01-01"), to = Sys.Date(), by = "month"),
+          bearer = BEARER,
+          output_location_type = "file",
+          file_dir = file.path(ntl_bm_dir, "FinalData", "VNP46A3_rasters"))
 
-for(year_i in years){
-
-  OUT_FILE <- file.path(ntl_bm_dir,
-                        "FinalData",
-                        "VNP46A4_rasters",
-                        paste0("bm_VNP46A4_",year_i,".tif"))
-  
-  if(!file.exists(OUT_FILE)){
-    r <- bm_raster(roi_sf = roi_sf,
-                   product_id = "VNP46A4",
-                   date = year_i,
-                   bearer = BEARER)
-    
-    writeRaster(r, OUT_FILE)
-  }
-}
-
-# Download monthly data --------------------------------------------------------
-# Downloads a raster for each month. If raster already created, skips calling 
-# function.
-
-months <- seq.Date(from = ymd("2022-01-01"),
-                   to = floor_date(Sys.Date(), unit = "months"),
-                   by = "month") %>%
-  as.character()
-
-for(month_ymd in months){
-  
-  year  <- month_ymd %>% year()
-  month <- month_ymd %>% month()
-  
-  OUT_FILE <- file.path(ntl_bm_dir,
-                        "FinalData",
-                        "VNP46A3_rasters",
-                        paste0("bm_VNP46A3_",year,"_",pad2(month),".tif"))
-  
-  if(!file.exists(OUT_FILE)){
-    r <- bm_raster(roi_sf = roi_sf,
-                   product_id = "VNP46A3",
-                   date = month_ymd,
-                   bearer = BEARER)
-    
-    writeRaster(r, OUT_FILE)
-  }
-}
-
-
-# Download daily data --------------------------------------------------------
-# * Downloads a raster for each daily If raster already created, skips calling 
-#   function.
-# * Some days do not have data, which will cause an error. Use trycatch to skip
-# * Both VNP46A1 and VNP46A2 are daily data. VNP46A2 has more corrections, but
-#   VNP46A1 has more recent data. We download both.
-
-dates <- seq.Date(from = ymd("2023-01-01"),
-                  to = Sys.Date(),
-                  by = "day") %>%
-  as.character() %>%
-  rev()
-
-for(date in dates){
-  for(product_id in c("VNP46A2")){ # , "VNP46A1"
-    
-    year <- date %>% year()
-    day  <- date %>% yday()
-    
-    OUT_FILE <- file.path(ntl_bm_dir,
-                          "FinalData",
-                          paste0(product_id, "_rasters"),
-                          paste0("bm_",product_id,"_",year,"_",pad3(day),".tif"))
-    
-    if(!file.exists(OUT_FILE)){
-      
-      print(OUT_FILE)
-      
-      out <- tryCatch(
-        {
-          
-          r <- bm_raster(roi_sf = roi_sf,
-                         product_id = product_id,
-                         date = date,
-                         bearer = BEARER)
-          
-          writeRaster(r, OUT_FILE)
-        },
-        error=function(cond) {
-          print("Error! Skipping.")
-        }
-      )
-      
-    }
-  }
-}
-
+## Daily Data
+bm_raster(roi_sf = roi_sf,
+          product_id = "VNP46A2",
+          date = seq.Date(from = ymd("2023-01-01"), to = Sys.Date(), by = "day"),
+          bearer = BEARER,
+          output_location_type = "file",
+          file_dir = file.path(ntl_bm_dir, "FinalData", "VNP46A2_rasters"))
--- notebooks/nighttime-lights/03_aggregate.R
+++ notebooks/nighttime-lights/03_aggregate.R
@@ -1,12 +1,13 @@
 # Aggregate Nighttime Lights
 
-# Do if make changes to how data processed and need to start from scratch
-DELETE_DIR <- F 
+# Create datasets at ADM 0, 1 and 2 levels with:
+# - Average NTL
+# - Median NTL
+# - Proportion of NTL above 2, 5 and 10
+# - The above, but excluding and only including gas flaring locations
 
-if(DELETE_DIR){
-  unlink(file.path(ntl_bm_dir, "FinalData", "aggregated"), recursive = T)
-  dir.create(file.path(ntl_bm_dir, "FinalData", "aggregated"))
-}
+# Saves a separate dataset for each admin unit and date. The next script
+# (04_append.R) appends datasets together.
 
 # Load/prep gas flaring boundaries data ----------------------------------------
 # Make spatial file for:
@@ -25,108 +26,124 @@ gf_sp <- gf_sf %>% as("Spatial")
 #### Country file
 adm0_sp <- readOGR(dsn = file.path(adm_dir, "tur_polbnda_adm0.shp"))
 
-#### Non GS Locations
+#### Non Gas-Flaring Locations
 adm0_no_gf_sp <- gDifference(adm0_sp, gf_sp, byid=F)
 adm0_no_gf_sp$id <- 1
 
-# Loop through ROIs ------------------------------------------------------------
-for(roi in c("tessellation", "adm0", "adm1", "adm2")){
-  
-  ## Load ROI spatial file
-  if(roi == "tessellation") roi_sf <- read_sf(file.path(tess_dir, "tessellation.geojson"))
-  
+# Aggregate NTL ================================================================
+# 1. Loop through ADMs (0, 1 and 2)
+# 2. Loop through products (daily, monthly, and annual)
+# 3. Loop through rasters (eg, for daily data, loop through raster for each day)
+# [If data for that date has not been extracted, go to 4 and 5; otherwise, skip]
+# 4. Extract NTL (average, median, proportion above thresholds)
+# 5. Save dataset
+
+# Loop through ADMs ------------------------------------------------------------
+for(roi in c("adm0", "adm1", "adm2")){
+
   if(roi == "adm0"){
-    roi_sf <- readOGR(dsn = file.path(adm_dir, "tur_polbnda_adm0.shp")) %>% 
+    roi_sf <- readOGR(dsn = file.path(adm_dir, "tur_polbnda_adm0.shp")) %>%
       st_as_sf() %>%
       dplyr::select(adm0)
-  } 
-  
+  }
+
   if(roi == "adm1"){
-    roi_sf <- readOGR(dsn = file.path(adm_dir, "tur_polbnda_adm1.shp")) %>% 
+    roi_sf <- readOGR(dsn = file.path(adm_dir, "tur_polbnda_adm1.shp")) %>%
       st_as_sf() %>%
       dplyr::select(adm1)
-  } 
-  
+  }
+
   if(roi == "adm2"){
-    roi_sf <- readOGR(dsn = file.path(adm_dir, "tur_polbna_adm2.shp")) %>% 
+    roi_sf <- readOGR(dsn = file.path(adm_dir, "tur_polbna_adm2.shp")) %>%
       st_as_sf() %>%
       dplyr::select(pcode)
-  } 
-  
+  }
+
   # Loop through product -------------------------------------------------------
   # VNP46A2 = daily
   # VNP46A3 = monthly
   # VNP46A4 = annually
-  
+
   for(product in c("VNP46A2", "VNP46A3", "VNP46A4")){
-    
+
     ## Make directory to export files - organized by ROI and prduct
     OUT_DIR <- file.path(ntl_bm_dir, "FinalData", "aggregated", paste0(roi, "_", product))
     dir.create(OUT_DIR)
-    
+
     # Loop through rasters -----------------------------------------------------
-    r_name_vec <- file.path(ntl_bm_dir, "FinalData", paste0(product, "_rasters")) %>% list.files()
-    
+    # Grab raster files of specific product (eg, all monthly files)
+    r_name_vec <- file.path(ntl_bm_dir, "FinalData", paste0(product, "_rasters")) %>%
+      list.files()
+
     for(r_name_i in r_name_vec){
-      
+
+      ## Name of file to export
       OUT_FILE <- file.path(OUT_DIR, r_name_i %>% str_replace_all(".tif", ".Rds"))
-      
-      ## Check if file exists
+
+      ## Check if file exists; if already exists, skip
       if(!file.exists(OUT_FILE)){
-        
+
         ## Load raster and create rasters for just gas flaring and non gas flaring locations
         r <- raster(file.path(ntl_bm_dir, "FinalData", paste0(product, "_rasters"), r_name_i))
-        
+
         r_gf   <- r %>% crop(gf_sp)         %>% mask(gf_sp)
         r_nogf <- r %>% crop(adm0_no_gf_sp) %>% mask(adm0_no_gf_sp)
-        
-        ## Extract data
+
+        ## Extract average and median NTL
         roi_sf$ntl_bm_mean       <- exact_extract(r,      roi_sf, fun = "mean")
         roi_sf$ntl_bm_gf_mean    <- exact_extract(r_gf,   roi_sf, fun = "mean")
         roi_sf$ntl_bm_no_gf_mean <- exact_extract(r_nogf, roi_sf, fun = "mean")
-        
+
         roi_sf$ntl_bm_median       <- exact_extract(r,      roi_sf, fun = "median")
         roi_sf$ntl_bm_gf_median    <- exact_extract(r_gf,   roi_sf, fun = "median")
         roi_sf$ntl_bm_no_gf_median <- exact_extract(r_nogf, roi_sf, fun = "median")
-        
+
+        ## Extract proportion of NTL that are above some threshold
         for(thresh in c(2, 5, 10)){
-          
+
           r_t <- r
           r_t[] <- as.numeric(r[] > thresh)
-          
+
           r_t_gf   <- r_t %>% crop(gf_sp)         %>% mask(gf_sp)
           r_t_nogf <- r_t %>% crop(adm0_no_gf_sp) %>% mask(adm0_no_gf_sp)
-          
+
           roi_sf[[paste0("ntl_bm_prop_g", thresh)]]       <- exact_extract(r_t,      roi_sf, fun = "mean")
           roi_sf[[paste0("ntl_bm_gf_prop_g", thresh)]]    <- exact_extract(r_t_gf,   roi_sf, fun = "mean")
           roi_sf[[paste0("ntl_bm_no_gf_prop_g", thresh)]] <- exact_extract(r_t_nogf, roi_sf, fun = "mean")
-          
+
         }
-        
+
         ## Prep for export
         roi_df <- roi_sf
         roi_df$geometry <- NULL
-        
+
         ## Add date
         if(product == "VNP46A2"){
-          year <- r_name_i %>% substring(12,15) %>% as.numeric()
-          day  <- r_name_i %>% substring(17,19) %>% as.numeric()
-          date_r <- as.Date(day, origin = paste0(year, "-01-01"))
+          date_r <- r_name_i %>%
+            str_replace_all(".*_t", "") %>%
+            str_replace_all(".tif", "") %>%
+            str_replace_all("_", "-") %>%
+            ymd()
         }
-        
+
         if(product == "VNP46A3"){
-          year <- r_name_i %>% substring(12,15) %>% as.numeric()
-          month  <- r_name_i %>% substring(17,19) %>% as.numeric()
-          date_r <- paste0(year, "-", month, "-01") %>% ymd()
+          date_r <- r_name_i %>%
+            str_replace_all(".*_t", "") %>%
+            str_replace_all(".tif", "") %>%
+            str_replace_all("_", "-") %>%
+            paste0("-01") %>%
+            ymd()
         }
-        
+
         if(product == "VNP46A4"){
-          # Just grab year
-          date_r <- r_name_i %>% substring(12,15) %>% as.numeric()
-        }      
-        
+          date_r <- r_name_i %>%
+            str_replace_all(".*_t", "") %>%
+            str_replace_all(".tif", "") %>%
+            as.numeric()
+        }
+
         roi_df$date <- date_r
-        
+
         ## Export
         saveRDS(roi_df, OUT_FILE)
       }
